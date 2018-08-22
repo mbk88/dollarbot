ab03cad8616899d67d856f6139fc0310cc76fd07
mbk88 <balbi.miguel@gmail.com>
2018-08-22 04:50:55 -0300

    bot code refactor, additions like YAML config file and business logic

--- dollarbot.py
+++ dollarbot.py
@@ -1,34 +1,57 @@
 from bs4 import BeautifulSoup
-import urllib3
-import re
 import requests
 import json
-import threading
+import time
+config_file = "conf.yml"
 
-def dollar_poller():
-    dolar_venta = 0
-    threading.Timer(900.0, dollar_poller).start()
-    http = urllib3.PoolManager()
-    r = http.request('GET', 'http://dolarhoy.bullmarketbrokers.com/')
-    soup = BeautifulSoup(r.data, 'html.parser')
-    soupc = soup.find("h4")
-    soupv = soupc.findNext("h4")
-    dolar_c = re.search(r'\d\d\.\d\d', str(soupc)).group(0)
-    dolar_v = re.search(r'\d\d\.\d\d', str(soupv)).group(0)
-    dif = dolar_venta - float(dolar_v)
+def poller():
+    r = requests.get('http://dolarhoy.bullmarketbrokers.com/')
+    soup = BeautifulSoup(r.content, 'html.parser')
+    soup_b = soup.find("h4")
+    soup_s = soup_b.findNext("h4")
+    buy = get_num(soup_b.getText())
+    sell = get_num(soup_s.getText())
 
-    if float(dolar_v) > dolar_venta:
-        post_dollar_in_slack(dolar_c, dolar_v)
-        dolar_venta = dolar_v
-    elif dif > 0.10 or dif < -0.10:
-        post_dollar_in_slack(dolar_c, dolar_v)
-        dolar_venta = dolar_v
+    config = read_conf()
+    last_price = float(config[0])
+    threshold = float(config[1])
+    difference = sell - last_price
+    percentage = (difference / last_price) * 100.
+    if percentage > threshold:
+        post(buy, sell, "Up", percentage)
+        save_last_price(sell)
+    elif percentage < -threshold:
+        post(buy, sell, "Down", percentage)
+        save_last_price(sell)
 
-def post_dollar_in_slack(compra, venta):
+# returns a float from a given string -- works if there's only 1 dot in the string
+def get_num(s):
+    return float(''.join(char for char in s if char.isdigit() or char == '.'))
+
+# saves last_price on the file's first line -- TODO fix issues with newlines
+def save_last_price(sell):
+    data = read_conf()
+    data[0] = str(sell)
+    with open(config_file, "w") as f:
+        for s in data:
+            f.write(s+'\n')
+
+# reads yaml configuration file and returns list with values
+def read_conf():
+    with open(config_file, "r") as f:
+        data = f.readlines()
+        # first line of the list would be last_price, second line would be percentage threshold
+    return data
+
+# posts dollar's buy and sell prices with % difference to Slack
+def post(buy, sell, status, perc):
     jason = {}
-    jason["text"] = "Compra: "+str(compra)+"  Venta: "+str(venta)
+    jason["text"] = "Buy: ${} Sell: ${} - Percentage: {:04.2f}% {}".format(buy, sell, perc, status)
     url = "https://hooks.slack.com/services/"
     r = requests.post(url, data=json.dumps(jason))
 
 
-dollar_poller()
+while True:
+    poller()
+    time.sleep(900)
+
